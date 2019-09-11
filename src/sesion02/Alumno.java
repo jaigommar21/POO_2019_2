dc7770a2bab9780bf4151ea9a3cfecde2ec5f764
jgomezm-tecsup <jgomezm@tecsup.edu.pe>
2019-09-11 14:36:44 -0500

    Update

---
+++ src/sesion02/Alumno.java
@@ -0,0 +1,29 @@
+package sesion02;
+/**
+* Esta clase se usa para 
+* representar a los alumnos
+*/
+public class Alumno{
+
+    private int codigo;
+
+    private String nombres ="Juan Pedro 2";
+
+    private String apellidos;
+
+    private double nota1;
+
+    private double nota2;
+
+    private String curso;
+
+    /**
+     * El metodo imprime el nombre del
+     * usuario
+     */
+    public void imprimir(){
+ 
+       System.out.println(nombres);
+    }
+
+}
---
+++ src/sesion02/Ejercicio1.java
@@ -0,0 +1,17 @@
+package sesion02;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Alumno pedro = new Alumno();
+	
+		
+		System.out.println("Hola Mundo");
+		
+		pedro.imprimir();
+		
+	}
+
+}
