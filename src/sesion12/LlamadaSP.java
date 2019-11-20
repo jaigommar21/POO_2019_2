8c19d8aee321682e2225ed9b081b795cc0b737c7
Jaime  Gomez Marin <jgomezm@tecsup.edu.pe>
2019-11-20 16:32:07 -0500

    sesion 13

---
+++ src/sesion12/LlamadaSP.java
@@ -0,0 +1,46 @@
+package sesion12;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class LlamadaSP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost/TECSUP?useSSL=false&serverTimezone=UTC";
+			String usuario = "root"; //
+			String clave = "123456"; //
+	
+			Connection con =  DriverManager.getConnection(url, usuario, clave);
+
+			String query ="{CALL CONTAR_ALUMNOS (?,?)} ";
+			CallableStatement cstatement = con.prepareCall(query);
+			cstatement.setInt("V_EDAD", 19);//Tipo entero
+			cstatement.registerOutParameter("V_NRO_ALUMNOS",Types.BINARY);
+			cstatement.execute();
+			int  nroAlumnos =cstatement.getInt(2);
+			
+			System.out.println(nroAlumnos);
+			
+			cstatement.close();   
+	
+			System.out.println("Conextion establecida");
+			con.close();			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
